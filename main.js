cfb65e46fb5aca3c17d2ac9f11f69ff116d95ffe
agustin <agutusco@gmail.com>
2024-05-22 18:11:38 -0300

    Cambio los formularios

--- main.js
+++ main.js
@@ -4,11 +4,27 @@ const images = carouselInner.querySelectorAll('img');
 let currentImageIndex = 0;
 
 function changeImage() {
-  images[currentImageIndex].style.opacity = 0; /* Fade out the current image */
+  images[currentImageIndex].style.opacity = 0; 
 
-  currentImageIndex = (currentImageIndex + 1) % images.length; /* Calculate the next image index */
+  currentImageIndex = (currentImageIndex + 1) % images.length;
 
-  images[currentImageIndex].style.opacity = 1; /* Fade in the next image */
+  images[currentImageIndex].style.opacity = 1;
 }
 
-setInterval(changeImage, 3000); /* Change images every 3 seconds */
+setInterval(changeImage, 3000);
+
+
+const contactoBtn = document.getElementById("icono1")
+const tasacionesBtn = document.getElementById("icono2")
+const contactoForm = document.getElementById("contactoForm");
+const tasacionesForm = document.getElementById("tasacionesForm");
+
+contactoBtn.addEventListener("click",() => {
+    contactoForm.classList.toggle("mostrar")
+} )
+
+tasacionesBtn.addEventListener("click",() => {
+  tasacionesForm.classList.toggle("mostrar")
+} )
+
+
